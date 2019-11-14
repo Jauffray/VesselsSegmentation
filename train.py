e97bcfb140420d01bfab4e80776edd053d868473
= <agaldran@gmail.com>
2019-11-14 14:33:33 +0000

    refactored data loading

--- train.py
+++ train.py
@@ -16,89 +16,135 @@ from tqdm import tqdm_notebook as tqdm
 import torch.nn as nn
 #import torch.nn.functional as F
 
-from utils import load_checkpoint, save_checkpoint, build_vesselsDataset, build_vesselsLoader
-from models.uNet import UNet 
+from utils.utils import load_checkpoint, save_checkpoint, build_vesselsDataset, build_vesselsLoader
+from models.uNet import UNet
+
+import argparse
+import torch
+
+# argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('--depth', default=5, help='depth of the network')
+parser.add_argument('--wf', default=6, help='number of filters in the first layer is 2**wf')
+parser.add_argument('--lr', default=0.001, help='Learning Rate')
+parser.add_argument('--batch_size', default=4, help='Batch Size')
+parser.add_argument('--experiment_path', default='experiments/my_experiment/',
+                    help='Where to store the resulting trained model')
+parser.add_argument('--data_path', default='DRIVE/', help='Where the training data is')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    # define here hyper-parameters that you do not want to expose to the command line
+    # or expose them in the arg parser, but give them a default value that you will not modify
+    n_epochs = 150
+    patience = 10
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # etc.
+
+    # gather parser parameters
+    d = args.depth
+    wf = args.wf
+    lr = args.lr
+    bs = args.batch_size
+    exp_path = args.experiment_path
+    data_path = args.data_path
+
+    print('* Create Dataloaders, for which you want to import get_train_val_loaders() from utils.get_loaders')
+    # your code here, you probably want to use bs
+    print('* Instantiate a model with depth {:d} and {:d} filters in the first layer'.format(d, 2**wf))
+    # your code here
+    model = None
+    print('* Instantiate an optimizer (adam, sgd, whatever you like the most) with a learning rate of {:4f}'.format(lr))
+    # your code here
+    optimizer = None
+    print('* Instantiate a loss function here')
+    # your code here
+    criterion = None
+
+    # etcetera etcetera, and finally train your model
+    print('-' * 10)
+    # train(model, n_epochs)
 
 
-train_dataset, dev_dataset = build_vesselsDataset()
-train_loader, dev_loader = build_vesselsLoader(train_dataset, dev_dataset, 1, 2)
-train_batch = next(iter(train_loader))
-dev_batch = next(iter(dev_loader))
-
-my_unet = UNet(n_channels = 3, n_classes=1)
-optimizer = optim.SGD(my_unet.parameters(), lr=0.001, momentum=0.9)
-#my_unet.cuda();
-criterion = nn.BCELoss()
-my_unet.train();
-
-lowest_loss = 10.0 #we set the loss treshold that starts the params save 
-try:           
-    checkpoint = load_checkpoint(my_unet, optimizer)
-    lowest_loss = checkpoint['loss']
-    epoch = checkpoint['epoch']
-except OSError:
-    print('No save found. \n') 
-#print(lowest_loss)
-    
-nr_epochs = 1
-train_print_rate = 1
-dev_print_rate = 1
-
-for epoch in tqdm(range(nr_epochs)):   # loop over the dataset multiple times
-    # train
-    my_unet.train()
-    running_loss = 0.0
-    for i, batches in enumerate(train_loader):
-        # get the inputs; data is a list of [inputs, labels]
-        image_batch, labels_batch = batches
-        #image_batch, labels_batch  =image_batch.cuda(), labels_batch.cuda()
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs_batch = my_unet(image_batch)
-        loss = criterion(outputs_batch, labels_batch)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % train_print_rate == 0:
-            print('[%d, %5d] lossTrain: %.3f' %
-                  (epoch + 1, i + 1, running_loss/train_print_rate)) 
-            running_loss = 0.0
-    # end of epoch
-    
-    # dev
-    my_unet.eval()
-    j = 0
-    total_loss = 0
-    for j, batches in enumerate(dev_loader):
-        # get the inputs; data is a list of [inputs, labels]
-        image_batch, labels_batch = batches
-        #image_batch, labels_batch = image_batch.cuda(), labels_batch.cuda()
-
-        # forward + NO BACKWARD / OPTIMIZE (validation, not training)
-        outputs_batch = my_unet(image_batch)
-        loss = criterion(outputs_batch, labels_batch) 
-                   
-        # print statistics
-        running_loss += loss.item()
-        total_loss += loss.item()
-        if j % dev_print_rate == 0:
-            print('[%d, %5d] lossVal: %.3f' %(epoch + 1, j + 1, running_loss/dev_print_rate)) 
-            running_loss = 0.0                        
-    
-    mean_loss = total_loss / (j + 1)
-    #print(j)
-    print('For this epoch: %d, the mean dev loss is %.3f.' %(epoch + 1, mean_loss))
-    
-    if (mean_loss < lowest_loss):
-        lowest_loss = mean_loss
-        save_checkpoint(my_unet, optimizer, mean_loss, epoch)
-        print('There is a new lowest loss : %.3f.' %mean_loss)
-    
-print('Finished Training, the lowest loss is %.3f.' %lowest_loss)
+# train_dataset, dev_dataset = build_vesselsDataset()
+# train_loader, dev_loader = build_vesselsLoader(train_dataset, dev_dataset, 1, 2)
+# train_batch = next(iter(train_loader))
+# dev_batch = next(iter(dev_loader))
+#
+# my_unet = UNet(n_channels = 3, n_classes=1)
+# optimizer = optim.SGD(my_unet.parameters(), lr=0.001, momentum=0.9)
+# #my_unet.cuda();
+# criterion = nn.BCELoss()
+# my_unet.train();
+#
+# lowest_loss = 10.0 #we set the loss treshold that starts the params save
+# try:
+#     checkpoint = load_checkpoint(my_unet, optimizer)
+#     lowest_loss = checkpoint['loss']
+#     epoch = checkpoint['epoch']
+# except OSError:
+#     print('No save found. \n')
+# #print(lowest_loss)
+#
+# nr_epochs = 1
+# train_print_rate = 1
+# dev_print_rate = 1
+#
+# for epoch in tqdm(range(nr_epochs)):   # loop over the dataset multiple times
+#     # train
+#     my_unet.train()
+#     running_loss = 0.0
+#     for i, batches in enumerate(train_loader):
+#         # get the inputs; data is a list of [inputs, labels]
+#         image_batch, labels_batch = batches
+#         #image_batch, labels_batch  =image_batch.cuda(), labels_batch.cuda()
+#         # zero the parameter gradients
+#         optimizer.zero_grad()
+#
+#         # forward + backward + optimize
+#         outputs_batch = my_unet(image_batch)
+#         loss = criterion(outputs_batch, labels_batch)
+#         loss.backward()
+#         optimizer.step()
+#
+#         # print statistics
+#         running_loss += loss.item()
+#         if i % train_print_rate == 0:
+#             print('[%d, %5d] lossTrain: %.3f' %
+#                   (epoch + 1, i + 1, running_loss/train_print_rate))
+#             running_loss = 0.0
+#     # end of epoch
+#
+#     # dev
+#     my_unet.eval()
+#     j = 0
+#     total_loss = 0
+#     for j, batches in enumerate(dev_loader):
+#         # get the inputs; data is a list of [inputs, labels]
+#         image_batch, labels_batch = batches
+#         #image_batch, labels_batch = image_batch.cuda(), labels_batch.cuda()
+#
+#         # forward + NO BACKWARD / OPTIMIZE (validation, not training)
+#         outputs_batch = my_unet(image_batch)
+#         loss = criterion(outputs_batch, labels_batch)
+#
+#         # print statistics
+#         running_loss += loss.item()
+#         total_loss += loss.item()
+#         if j % dev_print_rate == 0:
+#             print('[%d, %5d] lossVal: %.3f' %(epoch + 1, j + 1, running_loss/dev_print_rate))
+#             running_loss = 0.0
+#
+#     mean_loss = total_loss / (j + 1)
+#     #print(j)
+#     print('For this epoch: %d, the mean dev loss is %.3f.' %(epoch + 1, mean_loss))
+#
+#     if (mean_loss < lowest_loss):
+#         lowest_loss = mean_loss
+#         save_checkpoint(my_unet, optimizer, mean_loss, epoch)
+#         print('There is a new lowest loss : %.3f.' %mean_loss)
+#
+# print('Finished Training, the lowest loss is %.3f.' %lowest_loss)
 
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -9,86 +9,131 @@ Created on Tue Nov  5 19:55:39 2019
 #utils
 
 from PIL import Image
-import os.path as osp
-#from torchvision import transforms as tr
-from torch.utils.data.dataset import Dataset
-#from torch.utils.data import DataLoader
 import os
+import os.path as osp
+import numpy as np
+from skimage.measure import regionprops
+
 import torch
+from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
-from torchvision import transforms as tr
+#from torchvision import transforms as tr
+import paired_transforms_tv04 as p_tr
 
-PATH_TO_DATA = './data/'
-PATH_TO_SAVE = './experiments/params'
-PATH_IMS = PATH_TO_DATA + 'train/images'
-PATH_GT = PATH_TO_DATA + 'train/labels'
-PATH_VAL_IMS = PATH_TO_DATA + 'dev/images'
-PATH_VAL_GT = PATH_TO_DATA + 'dev/labels'
-#im_list = os.listdir(PATH_IMS)
-#gt_list = os.listdir(PATH_GT)
-#dev_im_list = os.listdir(PATH_VAL_IMS)
-#dev_gt_list = os.listdir(PATH_VAL_GT)
+class DRIVE(Dataset):
+    def __init__(self, path_to_data, mode='train', proportion=1, transforms=None, label_values=None):
+        if mode == 'train' or mode == 'dev':
+            self.path_to_data = osp.join(path_to_data, 'training')
+        elif mode == 'test':
+            self.path_to_data = osp.join(path_to_data, 'test')
 
-class VesselsDataset(Dataset):
-    def __init__(self, img_dir, gt_dir, transforms=None):
-        self.img_dir = img_dir
-        self.gt_dir = gt_dir
-        self.img_names = sorted(os.listdir(self.img_dir))
-        self.gt_names = sorted(os.listdir(self.gt_dir))
         self.transforms = transforms
+        self.im_list = sorted(os.listdir(osp.join(self.path_to_data, 'images')))
+        self.gt_list = sorted(os.listdir(osp.join(self.path_to_data, '1st_manual')))
+        self.mask_list = sorted(os.listdir(osp.join(self.path_to_data, 'mask')))
+        # proportion of images used; for test dataset, should be 1
+        num_ims = len(self.im_list)
+        if mode == 'train':
+            self.im_list = self.im_list[:int(proportion * num_ims)]
+            self.gt_list = self.gt_list[:int(proportion * num_ims)]
+            self.mask_list = self.mask_list[:int(proportion * num_ims)]
+        elif mode == 'dev':
+            self.im_list = self.im_list[int(proportion * num_ims):]
+            self.gt_list = self.gt_list[int(proportion * num_ims):]
+            self.mask_list = self.mask_list[int(proportion * num_ims):]
+        self.label_values = label_values  # for use in label_encoding
+
+    def label_encoding(self, gdt):
+        gdt_gray = np.array(gdt.convert('L'))
+        classes = np.arange(len(self.label_values))
+        for i in classes:
+            gdt_gray[gdt_gray == self.label_values[i]] = classes[i]
+        return Image.fromarray(gdt_gray)
+
+    def crop_to_fov(self, img, target, mask):
+        minr, minc, maxr, maxc = regionprops(np.array(mask))[0].bbox
+        im_crop = Image.fromarray(np.array(img)[minr:maxr, minc:maxc])
+        tg_crop = Image.fromarray(np.array(target)[minr:maxr, minc:maxc])
+        mask_crop = Image.fromarray(np.array(mask)[minr:maxr, minc:maxc])
+        return im_crop, tg_crop, mask_crop
 
     def __getitem__(self, index):
-        img = Image.open(osp.join(self.img_dir, self.img_names[index]))
-        gt = Image.open(osp.join(self.gt_dir, self.gt_names[index]))
+        # load image and labels
+        img = Image.open(osp.join(self.path_to_data, 'images', self.im_list[index]))
+        target = Image.open(osp.join(self.path_to_data, '1st_manual', self.gt_list[index]))
+        mask = Image.open(osp.join(self.path_to_data, 'mask', self.mask_list[index]))
+        img, target, mask = self.crop_to_fov(img, target, mask)
+
+        target = self.label_encoding(target)
+
         if self.transforms is not None:
-            img = self.transforms(img)
-            gt = self.transforms(gt)
-        return img, gt
+            img, target = self.transforms(img, target)
+
+        return img, target
 
     def __len__(self):
-        return len(self.img_names)
+        return len(self.im_list)
 
-def build_vesselsDataset():
-    resz = tr.Resize([512,512])
-    to_tensor = tr.ToTensor()
-    
-    train_dataset = VesselsDataset(PATH_IMS, PATH_GT, transforms = tr.Compose([resz, to_tensor]))
-    dev_dataset = VesselsDataset(PATH_VAL_IMS, PATH_VAL_GT, transforms = tr.Compose([resz, to_tensor]))    
-    #add a test_dataset later
-    
+def build_vesselsDataset(path_data, train_proportion=.8):
+    train_dataset = DRIVE(path_data, mode='train', proportion=train_proportion, label_values=[0, 255])
+    dev_dataset = DRIVE(path_data, mode='dev', proportion=train_proportion, label_values=[0, 255])
+
+    # transforms
+    size = 512, 512
+    resize = p_tr.Resize(size)
+
+    rotate = p_tr.RandomRotation(degrees=45)
+    scale = p_tr.RandomAffine(degrees=0, scale=(0.95, 1.20))
+    transl = p_tr.RandomAffine(degrees=0, translate=(0.05, 0))
+    # either translate, rotate, or scale
+    scale_transl_rot = p_tr.RandomChoice([scale, transl, rotate])
+
+    h_flip = p_tr.RandomHorizontalFlip()
+    v_flip = p_tr.RandomVerticalFlip()
+
+    brightness, contrast, saturation, hue = 0.25, 0.25, 0.25, 0.01
+    jitter = p_tr.ColorJitter(brightness, contrast, saturation, hue)
+
+    tensorizer = p_tr.ToTensor()
+
+    train_transforms = p_tr.Compose([resize, scale_transl_rot, h_flip, v_flip, jitter, tensorizer])
+    train_dataset.transforms = train_transforms
+
+    dev_transforms = p_tr.Compose([resize, tensorizer])
+    dev_dataset.transforms = dev_transforms
+
     return train_dataset, dev_dataset
 
-def build_vesselsLoader(train_dataset, dev_dataset, train_batch_sz, dev_batch_sz):
-        #add test dataset later
-        train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_sz, num_workers=0, shuffle=True)
-        dev_loader = DataLoader(dataset=dev_dataset, batch_size=dev_batch_sz, num_workers=0, shuffle=True)
-        
-        return train_loader, dev_loader
-    
-# Print unet's state_dict
-#print("Unet's state_dict:")
-# for param_tensor in my_unet.state_dict():
-#     print(param_tensor, "\t", my_unet.state_dict()[param_tensor].size())
 
-# Print optimizer's state_dict
-#print("Optimizer's state_dict:")
-# for var_name in optimizer.state_dict():
-#     print(var_name, "\t", optimizer.state_dict()[var_name])
+def build_vesselsLoader(path_data, train_proportion=.8, train_batch_sz=4, dev_batch_sz=4):
+    train_dataset, dev_dataset = build_vesselsDataset(path_data, train_proportion=train_proportion)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_sz, num_workers=8, shuffle=True)
+    dev_loader = DataLoader(dataset=dev_dataset, batch_size=dev_batch_sz, num_workers=8, shuffle=False)
+
+    return train_loader, dev_loader
 
 
 #Load the best model and optimizer, also handling the lack of save
-
-def load_checkpoint(model, optimizer):
+def load_checkpoint(model, optimizer, PATH_TO_SAVE):
         checkpoint = torch.load(PATH_TO_SAVE)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         return checkpoint
 
 #create function save
-def save_checkpoint(my_unet, optimizer, loss, epoch):
+def save_checkpoint(my_unet, optimizer, loss, epoch, PATH_TO_SAVE):
     torch.save({
                 'epoch': epoch,
                 'model_state_dict': my_unet.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': loss
                 }, PATH_TO_SAVE)
+
+if __name__ == '__main__':
+    # some basic testing to see if this works properly
+    path_data = '../data/DRIVE'
+    train_loader, dev_loader = build_vesselsLoader(path_data, train_proportion=.8, train_batch_sz=4, dev_batch_sz=4)
+    print(len(train_loader.dataset), len(dev_loader.dataset))
+    ims, labels = next(iter(train_loader))
+    print(ims.shape, labels.shape)
